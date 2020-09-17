c289aa9228118f57f135f3c9055ef2998641d7d3
Kai Iyer <kaiiyer47@yahoo.in>
2020-09-17 09:13:24 +0530

    Automating README.md

---
+++ update/README.md
@@ -0,0 +1,1 @@
+Inspired from [Victoria Drake](https://github.com/victoriadrake/victoriadrake)
---
+++ update/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/mmcdole/gofeed"
+)
+
+func makeReadme(filename string) error {
+	fp := gofeed.NewParser()
+	feed, err := fp.ParseURL("https://blog.kaiiyer.ninja/rss.xml")
+	if err != nil {
+		log.Fatalf("error getting feed: %v", err)
+	}
+	// Get the latest item
+	blogItem := feed.Items[0]
+
+	rand.Seed(time.Now().UnixNano())
+	whoList := []string{"🦄", "🐣", "🦊", "🦔", "🦡", "🐹", "🐷", "🌮", "🍝", "🥑"}
+	who := rand.Intn(len(whoList))
+	whatList := []string{"👍", "🎉", "💕", "🤷", "👏", "🙌"}
+	what := rand.Intn(len(whatList))
+	date := time.Now().Format("2 Sep 2020")
+
+	// Whisk together static and dynamic content until stiff peaks form
+	hello := "### Hello! I’m Kai Iyer. 👋\n\n ![](https://visitor-badge.laobi.icu/badge?page_id=kaiiyer.visitor-badge)	[![Twiiter Badge](https://img.shields.io/badge/@kaiiyer-blueviolet?style=flat-square&labelColor=1ca0f1&logo=twitter&logoColor=white&link=https://twitter.com/kaiiyer)](https://twitter.com/kaiiyer)	[![Medium Badge](https://img.shields.io/badge/@kaiiyer-black?style=flat-square&labelColor=00000&logo=medium&logoColor=white&link=https://medium.com/@kaiiyer)](https://medium.com/@kaiiyer)	[![Pinterest Badge](https://img.shields.io/badge/@kai_iyer-darkred?style=flat-square&labelColor=red&logo=Pinterest&logoColor=white&link=https://www.pinterest.com/kai_iyer/)](https://www.pinterest.com/kai_iyer/) \n\nNice to meet you! I'm currently exploring the ~~trends in the Cyber Market~~ anime world"
+	blog := "\n\n ![](https://github-readme-stats.vercel.app/api?username=kaiiyer&count_private=true&theme=dark&show_icons=true) <img src='https://media.giphy.com/media/dlMIwDQAxXn1K/giphy.gif' alt='You found me!' width='37%' align='right'/> \n\n I’m an Infosec developer :zap: and Privacy Advocate. I develop my skills and continuously push my limits.\n\n - I attend hackathons and speak about trending technologies.\n\n - This " + whoList[who] + " says they " + whatList[what] + " my latest blog post: **[" + blogItem.Title + "](" + blogItem.Link + ")**"
+	updated := "Last updated by magic on " + date + "."
+	data := fmt.Sprintf("%s\n%s\n\n%s\n", hello, blog, updated)
+
+	// Creating new file
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Bake at n bytes per second until golden brown
+	_, err = io.WriteString(file, data)
+	if err != nil {
+		return err
+	}
+	return file.Sync()
+}
+
+func main() {
+
+	makeReadme("../../README.md")
+
+}
